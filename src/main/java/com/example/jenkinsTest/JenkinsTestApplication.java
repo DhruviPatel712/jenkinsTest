00ff6a67e7f2c14f440fab0ae24bf4c7bea1b8d3
Dhruvipatel712 <dhruvi.patel@sjintelligence.in>
2023-09-30 13:28:32 +0530

    change file

--- src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
@@ -2,13 +2,21 @@ package com.example.jenkinsTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class JenkinsTestApplication {
 
+
+
+
+	@RequestMapping(value = "/")
+	public String testWeb() {
+		return "Hello from jenkins";
+	}
 	public static void main(String[] args) {
 
-		System.err.println("hello jenkins!!!");
+
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 
